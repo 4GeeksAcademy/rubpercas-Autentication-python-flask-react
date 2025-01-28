7a0d409446ebaa431a28127e2262268c82e50922
Rubén Pérez Castrillo <ruben.perez.castrillo@gmail.com>
2025-01-28 19:45:14 +0000

    private route solved

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,21 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
-	const Navigate = useNavigate()
+	const Navigate = useNavigate();
+
 	function handleLogout() {
-		actions.logout()
-		Navigate('/')
-	}
+		actions.logout();
+		Navigate('/');
+	};
+
 	function handlePrivate() {
-		Navigate('/private')
-	}
+		Navigate('/private');
+	};
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            store.auth = true;
+        } else {
+            store.auth = false;
+        }
+    }, [actions]);
+
 	return (
 		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+			<div className="container-fluid d-flex justify-content-between m-0">
 				<h5>AUTENTICACION</h5>
 				<div className="ml-auto">
 					{store.auth === true ?
